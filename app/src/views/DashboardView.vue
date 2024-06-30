292efaf136391bd86b61ef7ff826a33951114c0c
Thalida Noel <hello@thalida.com>
2024-06-30 20:29:04 +0000

    Setup main app layout

--- app/src/views/DashboardView.vue
+++ app/src/views/DashboardView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-// import TheWelcome from '../components/TheWelcome.vue'
+import AppLayout from '@/layouts/AppLayout.vue'
 </script>
 
 <template>
-  <main class="flex h-full max-h-full flex-col bg-blue-100 dark:bg-slate-900">
-    <!-- <TheWelcome /> -->
-  </main>
+  <AppLayout> Hello! </AppLayout>
 </template>
