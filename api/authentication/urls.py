ec4a2e1f72ab2d13f89829c7481bc85a4ec9caf0
Thalida Noel <hello@thalida.com>
2024-06-30 05:19:43 +0000

    Update auth urls format

--- api/authentication/urls.py
+++ api/authentication/urls.py
@@ -15,28 +15,45 @@ router = routers.SimpleRouter()
 
 router.register(r"users", UserViewSet, basename="users")
 
-drf_socialauth_urlpatterns = (
-    [
-        re_path(r"^authorize/?$", AuthorizationView.as_view(), name="authorize"),
-        re_path(r"^token/?$", TokenView.as_view(), name="token"),
-        re_path(r"^convert-token/?$", ConvertTokenView.as_view(), name="convert_token"),
-        re_path(r"^revoke-token/?$", RevokeTokenView.as_view(), name="revoke_token"),
-        re_path(
-            r"^invalidate-sessions/?$",
-            InvalidateSessions.as_view(),
-            name="invalidate_sessions",
-        ),
-        re_path(
-            r"^invalidate-refresh-tokens/?$",
-            InvalidateRefreshTokens.as_view(),
-            name="invalidate_refresh_tokens",
-        ),
-        re_path("", include("social_django.urls", namespace="social")),
-    ],
-    "drf",
-)
+# drf_socialauth_urlpatterns = (
+#     [
+#         re_path(r"^authorize/?$", AuthorizationView.as_view(), name="authorize"),
+#         re_path(r"^token/?$", TokenView.as_view(), name="token"),
+#         re_path(r"^convert-token/?$", ConvertTokenView.as_view(), name="convert_token"),
+#         re_path(r"^revoke-token/?$", RevokeTokenView.as_view(), name="revoke_token"),
+#         re_path(
+#             r"^invalidate-sessions/?$",
+#             InvalidateSessions.as_view(),
+#             name="invalidate_sessions",
+#         ),
+#         re_path(
+#             r"^invalidate-refresh-tokens/?$",
+#             InvalidateRefreshTokens.as_view(),
+#             name="invalidate_refresh_tokens",
+#         ),
+#         re_path("", include("social_django.urls", namespace="social")),
+#     ],
+#     "drf",
+# )
+
+drf_socialauth_urlpatterns = [
+    re_path(r"^authorize/?$", AuthorizationView.as_view(), name="authorize"),
+    re_path(r"^token/?$", TokenView.as_view(), name="token"),
+    re_path(r"^convert-token/?$", ConvertTokenView.as_view(), name="convert_token"),
+    re_path(r"^revoke-token/?$", RevokeTokenView.as_view(), name="revoke_token"),
+    re_path(
+        r"^invalidate-sessions/?$",
+        InvalidateSessions.as_view(),
+        name="invalidate_sessions",
+    ),
+    re_path(
+        r"^invalidate-refresh-tokens/?$",
+        InvalidateRefreshTokens.as_view(),
+        name="invalidate_refresh_tokens",
+    ),
+]
 
 urlpatterns = [
     re_path(r"^", include(router.urls)),
-    re_path(r"^auth/", include(drf_socialauth_urlpatterns)),
+    re_path(r"^auth/", include(drf_socialauth_urlpatterns), name="drf"),
 ]
