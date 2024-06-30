a8726ab9a3c5f8c2e71f7039e13a31c9b02272d3
Thalida Noel <hello@thalida.com>
2024-06-30 16:56:07 +0000

    Organize settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -23,12 +23,13 @@ from docs.utils import read_file
 
 load_dotenv()
 
+# ============================================================================ #
+#                                                                              #
+#   BASE SETTINGS
+#                                                                              #
+# ============================================================================ #
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-#^"
@@ -36,50 +37,15 @@ SECRET_KEY = "django-insecure-#^"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CORS_ALLOWED_ORIGIN_REGEXES = []
+WSGI_APPLICATION = "api.wsgi.application"
 
-if DEBUG:
-    ALLOWED_HOSTS += [
-        "localhost",
-        "127.0.0.1",
-        "localhost:3000",
-        "127.0.0.1:8000",
-    ]
 
-    CORS_ALLOWED_ORIGIN_REGEXES += [
-        r"^http(s)?://localhost:3000$",
-        r"^http(s)?://127.0.0.1:3000$",
-    ]
-
-# User & Authentication
-AUTH_USER_MODEL = "authentication.User"
-
-ACTIVATE_JWT = True
-SOCIAL_AUTH_JSONFIELD_ENABLED = True
-# Use email as core login field
-SOCIAL_AUTH_USERNAME_IS_FULL_EMAIL = True
-
-SOCIAL_AUTH_PIPELINE = (
-    # Order matters here
-    "social_core.pipeline.social_auth.social_details",
-    "social_core.pipeline.social_auth.social_uid",
-    "social_core.pipeline.social_auth.social_user",
-    "social_core.pipeline.user.get_username",
-    "social_core.pipeline.social_auth.associate_by_email",
-    "social_core.pipeline.user.create_user",
-    "social_core.pipeline.social_auth.associate_user",
-    "social_core.pipeline.social_auth.load_extra_data",
-    "social_core.pipeline.user.user_details",
-)
-
-AUTHENTICATION_BACKENDS = (
-    "drf_social_oauth2.backends.DjangoOAuth2",
-    "django.contrib.auth.backends.ModelBackend",
-)
-
+# ============================================================================ #
+#                                                                              #
+#   INSTALLED APPS
+#                                                                              #
+# ============================================================================ #
 # Application definition
-
 INSTALLED_APPS = [
     "unfold",  # before django.contrib.admin
     "unfold.contrib.filters",  # optional, if special filters are needed
@@ -107,71 +73,59 @@ INSTALLED_APPS = [
     "organizations",
 ]
 
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
 
-DEFAULT_RENDERER_CLASSES = [
-    "rest_framework.renderers.JSONRenderer",
-]
+# ============================================================================ #
+#                                                                              #
+#   HOST SETTINGS
+#                                                                              #
+# ============================================================================ #
+ALLOWED_HOSTS = []
+CORS_ALLOWED_ORIGIN_REGEXES = []
 
 if DEBUG:
-    DEFAULT_RENDERER_CLASSES += [
-        "rest_framework.renderers.BrowsableAPIRenderer",
+    ALLOWED_HOSTS += [
+        "localhost",
+        "127.0.0.1",
+        "localhost:3000",
+        "127.0.0.1:8000",
     ]
 
-REST_FRAMEWORK = {
-    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_RENDERER_CLASSES": DEFAULT_RENDERER_CLASSES,
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.BasicAuthentication",
-        "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
-        "drf_social_oauth2.authentication.SocialAuthentication",
-    ],
-    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
-}
-
-ROOT_URLCONF = "api.urls"
-
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [f"{BASE_DIR}/templates"],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-                # 3rd party
-                "social_django.context_processors.backends",
-                "social_django.context_processors.login_redirect",
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = "api.wsgi.application"
+    CORS_ALLOWED_ORIGIN_REGEXES += [
+        r"^http(s)?://localhost:3000$",
+        r"^http(s)?://127.0.0.1:3000$",
+    ]
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+# ============================================================================ #
+#                                                                              #
+#   AUTHENTICATION SETTINGS
+#                                                                              #
+# ============================================================================ #
+# User & Authentication
+AUTH_USER_MODEL = "authentication.User"
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default="postgresql://postgres:postgres@localhost:5432/postgres",
-        conn_max_age=600,
-    )
-}
+ACTIVATE_JWT = True
+SOCIAL_AUTH_JSONFIELD_ENABLED = True
+# Use email as core login field
+SOCIAL_AUTH_USERNAME_IS_FULL_EMAIL = True
 
+SOCIAL_AUTH_PIPELINE = (
+    # Order matters here
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.social_auth.associate_by_email",
+    "social_core.pipeline.user.create_user",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+)
+
+AUTHENTICATION_BACKENDS = (
+    "drf_social_oauth2.backends.DjangoOAuth2",
+    "django.contrib.auth.backends.ModelBackend",
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -192,8 +146,95 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
+# ============================================================================ #
+#                                                                              #
+#   MIDDLEWARE & TEMPLATES
+#                                                                              #
+# ============================================================================ #
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [f"{BASE_DIR}/templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                # 3rd party
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect",
+            ],
+        },
+    },
+]
+
+
+# ============================================================================ #
+#                                                                              #
+#   API: REST FRAMEWORK SETTINGS
+#                                                                              #
+# ============================================================================ #
+ROOT_URLCONF = "api.urls"
+
+DEFAULT_RENDERER_CLASSES = [
+    "rest_framework.renderers.JSONRenderer",
+]
+if DEBUG:
+    DEFAULT_RENDERER_CLASSES += [
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_RENDERER_CLASSES": DEFAULT_RENDERER_CLASSES,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
+        "drf_social_oauth2.authentication.SocialAuthentication",
+    ],
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+}
+
+
+# ============================================================================ #
+#                                                                              #
+#   DATABASE SETTINGS
+#   https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+#
+#                                                                              #
+# ============================================================================ #
+DATABASES = {
+    "default": dj_database_url.config(
+        default="postgresql://postgres:postgres@localhost:5432/postgres",
+        conn_max_age=600,
+    )
+}
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# ============================================================================ #
+#                                                                              #
+#   INTERNATIONALIZATION
+#   https://docs.djangoproject.com/en/5.0/topics/i18n/
+#
+#                                                                              #
+# ============================================================================ #
 
 LANGUAGE_CODE = "en-us"
 
@@ -204,18 +245,25 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+# ============================================================================ #
+#                                                                              #
+#   STATIC FILES (CSS, JavaScript, Images)
+#   https://docs.djangoproject.com/en/5.0/howto/static-files/
+#
+#                                                                              #
+# ============================================================================ #
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# ============================================================================ #
+#                                                                              #
+#   SPECATULAR OPENAPI DOCUMENTATION
+#
+#                                                                              #
+# ============================================================================ #
 
 SPECTACULAR_SETTINGS = {
     "VERSION": "1.0.0",
@@ -249,6 +297,11 @@ SPECTACULAR_SETTINGS = {
 }
 
 
+# ============================================================================ #
+#                                                                              #
+#   UNFOLD DJANGO ADMIN
+#                                                                              #
+# ============================================================================ #
 UNFOLD = {
     "SITE_TITLE": "Admin",
     "SITE_HEADER": "Admin",
