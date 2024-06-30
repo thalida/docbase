101c98ee762a86be959b0d90b0654f2da156fbdd
Thalida Noel <hello@thalida.com>
2024-06-30 22:18:22 +0000

    Styled sidebare better

--- app/src/components/ui/AppSidebar.vue
+++ app/src/components/ui/AppSidebar.vue
@@ -1,23 +1,32 @@
 <script setup lang="ts">
+import { Cog8ToothIcon, FolderPlusIcon, SquaresPlusIcon } from '@heroicons/vue/24/outline'
+
+import { ref } from 'vue'
 import {
-  CalendarIcon,
-  ChartPieIcon,
-  DocumentDuplicateIcon,
-  FolderIcon,
-  HomeIcon,
-  UsersIcon
-} from '@heroicons/vue/24/outline'
+  Listbox,
+  ListboxButton,
+  ListboxLabel,
+  ListboxOption,
+  ListboxOptions
+} from '@headlessui/vue'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
 import AppThemeSwitcher from '@/components/ui/AppThemeSwitcher.vue'
 
-const navigation = [
-  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-  { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
-  { name: 'Reports', href: '#', icon: ChartPieIcon, current: false }
+const people = [
+  { id: 1, name: 'Wade Cooper' },
+  { id: 2, name: 'Arlene Mccoy' },
+  { id: 3, name: 'Devon Webb' },
+  { id: 4, name: 'Tom Cook' },
+  { id: 5, name: 'Tanya Fox' },
+  { id: 6, name: 'Hellen Schmidt' },
+  { id: 7, name: 'Caroline Schultz' },
+  { id: 8, name: 'Mason Heaney' },
+  { id: 9, name: 'Claudie Smitham' },
+  { id: 10, name: 'Emil Schaefer' }
 ]
-const teams = [
+
+const selected = ref(people[3])
+const databases = [
   { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
   { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
   { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false }
@@ -26,7 +35,7 @@ const teams = [
 
 <template>
   <div
-    class="flex grow flex-col gap-y-5 overflow-y-auto bg-white border-r border-gray-200 px-6 pb-2 dark:bg-gray-900 dark:border-0 :ring-1 dark:ring-white/10"
+    class="flex grow flex-col gap-y-5 overflow-y-auto bg-white border-r border-gray-200 px-4 pb-2 dark:bg-gray-900 dark:border-0 :ring-1 dark:ring-white/10"
   >
     <div class="flex flex-row justify-between h-16 shrink-0 items-center">
       <div>
@@ -41,55 +50,110 @@ const teams = [
     <nav class="flex flex-1 flex-col">
       <ul role="list" class="flex flex-1 flex-col gap-y-7">
         <li>
-          <ul role="list" class="-mx-2 space-y-1">
-            <li v-for="item in navigation" :key="item.name">
-              <a
-                :href="item.href"
-                :class="[
-                  item.current
-                    ? 'bg-gray-50 text-indigo-600 dark:bg-gray-800 dark:text-white'
-                    : 'text-gray-700 hover:bg-gray-50 hover:text-indigo-600 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white',
-                  'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6'
-                ]"
+          <Listbox as="div" v-model="selected">
+            <div class="flex flex-row justify-between items-center">
+              <ListboxLabel class="flex-grow text-sm font-semibold leading-6 text-gray-400"
+                >Workspaces</ListboxLabel
               >
-                <component
-                  :is="item.icon"
-                  :class="[
-                    item.current
-                      ? 'text-indigo-600 dark:text-white'
-                      : 'text-gray-400 group-hover:text-indigo-600 dark:text-white dark:group-hover:text-white',
-                    'h-6 w-6 shrink-0'
-                  ]"
-                  aria-hidden="true"
-                />
-                {{ item.name }}
-              </a>
-            </li>
-          </ul>
+              <div>
+                <button
+                  type="button"
+                  class="flex flex-row items-center justify-between px-2.5 py-2 rounded-md text-sm font-semibold text-yellow-700 dark:text-yellow-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+                >
+                  <FolderPlusIcon class="-ml-0.5 h-5 w-5" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+            <div class="flex flex-row items-center justify-center gap-2 mt-2">
+              <div class="relative flex-grow">
+                <ListboxButton
+                  class="relative w-full cursor-default rounded-md bg-white dark:bg-gray-900 py-1.5 pl-3 pr-10 text-left text-gray-900 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-white/10 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                >
+                  <span class="block truncate">{{ selected.name }}</span>
+                  <span
+                    class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
+                  >
+                    <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                  </span>
+                </ListboxButton>
+
+                <transition
+                  leave-active-class="transition ease-in duration-100"
+                  leave-from-class="opacity-100"
+                  leave-to-class="opacity-0"
+                >
+                  <ListboxOptions
+                    class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white dark:bg-gray-900 py-1 text-base shadow-lg ring-1 ring-black dark:ring-white/10 ring-opacity-5 focus:outline-none sm:text-sm"
+                  >
+                    <ListboxOption
+                      as="template"
+                      v-for="person in people"
+                      :key="person.id"
+                      :value="person"
+                      v-slot="{ active, selected }"
+                    >
+                      <li
+                        :class="[
+                          active ? 'bg-indigo-600 text-white' : 'text-gray-900 dark:text-white',
+                          'relative cursor-default select-none py-2 pl-3 pr-9'
+                        ]"
+                      >
+                        <span
+                          :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']"
+                          >{{ person.name }}</span
+                        >
+
+                        <span
+                          v-if="selected"
+                          :class="[
+                            active ? 'text-white' : 'text-indigo-600',
+                            'absolute inset-y-0 right-0 flex items-center pr-4'
+                          ]"
+                        >
+                          <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      </li>
+                    </ListboxOption>
+                  </ListboxOptions>
+                </transition>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  class="flex flex-row items-center justify-between px-2.5 py-2 rounded-md text-sm font-semibold text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+                >
+                  <Cog8ToothIcon class="h-5 w-5" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+          </Listbox>
         </li>
         <li>
-          <div class="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
+          <div class="flex flex-row justify-between items-center">
+            <div class="w-full flex-grow text-sm font-semibold leading-6 text-gray-400">
+              Databases
+            </div>
+            <div>
+              <button
+                type="button"
+                class="flex flex-row items-center justify-between px-2.5 py-2 rounded-md text-yellow-700 dark:text-yellow-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+              >
+                <SquaresPlusIcon class="-ml-0.5 h-5 w-5" aria-hidden="true" />
+              </button>
+            </div>
+          </div>
           <ul role="list" class="-mx-2 mt-2 space-y-1">
-            <li v-for="team in teams" :key="team.name">
+            <li v-for="db in databases" :key="db.name">
               <a
-                :href="team.href"
+                :href="db.href"
                 :class="[
-                  team.current
+                  db.current
                     ? 'bg-gray-50 text-indigo-600 dark:bg-gray-800 dark:text-white'
-                    : 'text-gray-700 hover:bg-gray-50 hover:text-indigo-600 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white',
+                    : 'text-gray-700 hover:bg-gray-50 hover:text-indigo-600 dark:text-white dark:hover:bg-gray-800 dark:hover:text-white',
                   'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6'
                 ]"
               >
-                <span
-                  :class="[
-                    team.current
-                      ? 'border-indigo-600 text-indigo-600 dark:border-gray-700 dark:text-gray-400'
-                      : 'border-gray-200 text-gray-400 group-hover:border-indigo-600 group-hover:text-indigo-600 dark:border-gray-700 dark:text-gray-400 dark:group-hover:border-gray-700 dark:group-hover:text-gray-400',
-                    'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border bg-white text-[0.625rem] font-medium'
-                  ]"
-                  >{{ team.initial }}</span
-                >
-                <span class="truncate">{{ team.name }}</span>
+                <span class="truncate">{{ db.name }}</span>
               </a>
             </li>
           </ul>
--- app/src/components/ui/AppThemeSwitcher.vue
+++ app/src/components/ui/AppThemeSwitcher.vue
@@ -3,24 +3,24 @@ import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { useUIStore } from '@/stores/ui'
 import type { Theme } from '@/types/ui'
 
-import { BIconMoonFill, BIconSunFill, BIconCircleHalf } from 'bootstrap-icons-vue'
+import { SunIcon, MoonIcon, ComputerDesktopIcon } from '@heroicons/vue/24/outline'
 
 const uiStore = useUIStore()
 
 const menuOptions = [
   {
     key: 'light',
-    icon: BIconSunFill,
+    icon: SunIcon,
     label: 'Light'
   },
   {
     key: 'dark',
-    icon: BIconMoonFill,
+    icon: MoonIcon,
     label: 'Dark'
   },
   {
     key: 'system',
-    icon: BIconCircleHalf,
+    icon: ComputerDesktopIcon,
     label: 'System'
   }
 ]
@@ -29,10 +29,12 @@ const menuOptions = [
 <template>
   <Menu as="div" class="relative inline-block text-left">
     <div>
-      <MenuButton class="flex items-center rounded-full text-blue-500 dark:text-slate-800">
+      <MenuButton
+        class="flex flex-row items-center justify-between px-2.5 py-2 rounded-md text-sm font-semibold text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+      >
         <span class="sr-only">Open options</span>
-        <BIconSunFill v-if="uiStore.colorScheme === 'light'" class="h-5 w-5" />
-        <BIconMoonFill v-else class="h-5 w-5" />
+        <SunIcon v-if="uiStore.colorScheme === 'light'" class="h-5 w-5" />
+        <MoonIcon v-else class="h-5 w-5" />
       </MenuButton>
     </div>
 
@@ -45,7 +47,7 @@ const menuOptions = [
       leave-to-class="scale-95 transform opacity-0"
     >
       <MenuItems
-        class="absolute top-5 right-1 z-10 mt-2 min-w-32 origin-top-right overflow-hidden rounded-md bg-blue-50 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-slate-800"
+        class="absolute top-8 right-1 z-10 mt-2 min-w-32 origin-top-right overflow-hidden rounded-md bg-white dark:bg-gray-900 shadow-lg ring-1 ring-black dark:ring-white/10 ring-opacity-5 focus:outline-none sm:text-sm"
       >
         <div>
           <MenuItem v-for="option in menuOptions" :key="option.key" v-slot="{ active: hover }">
@@ -53,10 +55,9 @@ const menuOptions = [
               @click="uiStore.setTheme(option.key as Theme)"
               class="flex w-full flex-row items-center justify-start gap-2 px-4 py-2 text-left text-sm"
               :class="{
-                'bg-teal-100 text-blue-500 dark:bg-slate-900/50 dark:text-blue-500':
-                  uiStore.theme === option.key,
-                'text-blue-500': uiStore.theme !== option.key && hover,
-                'text-gray-700 dark:text-white': uiStore.theme !== option.key && !hover
+                'text-indigo-600': uiStore.theme === option.key,
+                'bg-indigo-600 text-white': uiStore.theme !== option.key && hover,
+                'text-gray-900 dark:text-white': uiStore.theme !== option.key && !hover
               }"
             >
               <component :is="option.icon" class="mr-2 inline-block h-4 w-4" />
