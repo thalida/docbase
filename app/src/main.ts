a34f82de99cc6f7ff9cdf6fed310e9ddb2cc4801
Thalida Noel <hello@thalida.com>
2024-07-04 20:15:37 +0000

    primevue

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,13 +4,16 @@ import './main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import vue3GoogleLogin from 'vue3-google-login'
+
 import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+// import PrimeVue from 'primevue/config'
 import Tooltip from 'primevue/tooltip'
 
 import App from './App.vue'
 import router from './router'
 // @ts-ignore
-import AuraTheme from './themes/aura'
+// import AuraTheme from './themes/aura'
 
 const app = createApp(App)
 
@@ -20,9 +23,14 @@ app.use(vue3GoogleLogin, {
   clientId: import.meta.env.VITE_GOOGLE_OAUTH2_CLIENT_ID
 })
 
+// app.use(PrimeVue, {
+//   unstyled: true,
+//   pt: AuraTheme
+// })
 app.use(PrimeVue, {
-  unstyled: true,
-  pt: AuraTheme
+  theme: {
+    preset: Aura
+  }
 })
 app.directive('tooltip', Tooltip)
 
