1caf8a84d5f88fff20eb38907b3937384d4161f8
Thalida Noel <hello@thalida.com>
2024-07-04 20:24:47 +0000

    remove primevue

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,5 +7,7 @@ pre-commit install
 cd $WORKSPACE_FOLDER/app
 npm install
 
+npm i -g @quasar/cli
+
 chmod -R +x $WORKSPACE_FOLDER/scripts
 sudo ln -s $WORKSPACE_FOLDER/scripts/* /usr/local/bin
--- app-v1/src/main.ts
+++ app-v1/src/main.ts
@@ -5,15 +5,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import vue3GoogleLogin from 'vue3-google-login'
 
-import PrimeVue from 'primevue/config'
-import Aura from '@primevue/themes/aura'
-// import PrimeVue from 'primevue/config'
-import Tooltip from 'primevue/tooltip'
-
 import App from './App.vue'
 import router from './router'
-// @ts-ignore
-// import AuraTheme from './themes/aura'
 
 const app = createApp(App)
 
@@ -22,16 +15,5 @@ app.use(router)
 app.use(vue3GoogleLogin, {
   clientId: import.meta.env.VITE_GOOGLE_OAUTH2_CLIENT_ID
 })
-
-// app.use(PrimeVue, {
-//   unstyled: true,
-//   pt: AuraTheme
-// })
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura
-  }
-})
-app.directive('tooltip', Tooltip)
 
 app.mount('#app')
