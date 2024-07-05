421e0790f74dfbbc99293b425679d48f0feccb54
Thalida Noel <hello@thalida.com>
2024-07-05 02:07:03 +0000

    Fix login view

--- app/src/views/LoginView.vue
+++ app/src/views/LoginView.vue
@@ -37,25 +37,21 @@ async function handleGoogleLogin() {
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
             alt="Your Company"
           />
-          <h2 class="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <h2
+            class="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-white"
+          >
             Sign in to your account
           </h2>
-          <p class="mt-2 text-sm leading-6 text-gray-500">
-            Not a member?
-            {{ ' ' }}
-            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500"
-              >Start a 14 day free trial</a
-            >
-          </p>
         </div>
 
         <div
           v-if="error"
-          class="mt-4 flex flex-row gap-2 rounded-md bg-red-50 p-4 sm:mx-auto sm:w-full sm:max-w-sm dark:bg-red-200"
+          class="mt-4 flex flex-row items-center justify-start gap-2 rounded-md bg-red-50 p-4 sm:mx-auto sm:w-full sm:max-w-sm dark:bg-red-200"
         >
-          <div class="flex-shrink-0">
-            <XCircleIcon class="h-5 w-5 text-red-400 dark:text-red-600" aria-hidden="true" />
-          </div>
+          <i
+            class="flex-shrink-0 pi pi-times-circle text-red-400 dark:text-red-600"
+            aria-hidden="true"
+          />
           <p class="text-sm font-medium text-red-800">
             {{ error }}
           </p>
@@ -86,13 +82,6 @@ async function handleGoogleLogin() {
             </svg>
             <span class="text-sm font-semibold leading-6">Continue with Google</span>
           </button>
-
-          <!-- <a href="#" class="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent">
-            <svg class="h-5 w-5 fill-[#24292F]" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-              <path fill-rule="evenodd" d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z" clip-rule="evenodd" />
-            </svg>
-            <span class="text-sm font-semibold leading-6">GitHub</span>
-          </a> -->
         </div>
       </div>
     </div>
